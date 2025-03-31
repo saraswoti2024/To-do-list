8eebc8556bee8870b1b1f91faddc0fee95ce41f3
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-03-31 22:32:02 +0545

    adding time,date(date is only displayed per day

--- todo/app1/models.py
+++ todo/app1/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Textstore(models.Model):
     textmodel = models.TextField()
     isdelete = models.BooleanField(null=True,default=False)
+    date = models.DateField(null=True)
+    time = models.TimeField(null=True)
--- todo/app1/views.py
+++ todo/app1/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render,redirect
 from .models import *
 from django.contrib import messages
+from datetime import date,datetime
+from django.utils import timezone 
+import time
 
 # Create your views here.
 def home(request):
     datas = Textstore.objects.filter(isdelete=False).order_by('-id')
+    dates = timezone.now().date()
+    times = timezone.now().time()
     if request.method=='POST':
         data= request.POST
-        text = data.get('texts','')
+        text = data['texts']
         try:
-            datas = Textstore(textmodel=text)
+            datas = Textstore(textmodel=text,date=dates,time=times)
             datas.full_clean()
             datas.save()
             messages.success(request,'saved successfully!')
@@ -17,13 +22,20 @@ def home(request):
         except Exception as e:
             messages.error(request,f'{str(e)}')
             return redirect('home')
-    return render(request,'app1/home.html',{'datas':datas})
+    context={
+        'datas': datas,
+    }
+    return render(request,'app1/home.html',context)
 
 def edit(request,id):
     tasks = Textstore.objects.get(id=id)
+    dates = timezone.now().date()
+    times = timezone.now().time()
     if request.method=='POST':
         data = Textstore.objects.get(id=id)
         data.textmodel = request.POST['texts']
+        data.date = dates
+        data.time = times
         try:
             data.full_clean()
             data.save()
